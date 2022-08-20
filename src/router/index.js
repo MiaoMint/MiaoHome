54f27480fa3452ed2d254f1ca0a71ab3fa2f11df
ERHECY <1981324730@qq.com>
2022-08-20 22:08:05 +0800

    修复：修改history模式为hash模式解决vercel刷新404问题

--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,10 @@
-import { createRouter, createWebHistory } from 'vue-router'
+import { createRouter, createWebHashHistory } from 'vue-router'
 import HomeView from '../views/HomeView.vue'
 import GithubView from '../views/GithubView.vue'
 import BlogView from '../views/BlogView.vue'
 
 const router = createRouter({
-  history: createWebHistory(import.meta.env.BASE_URL),
+  history: createWebHashHistory(),
   routes: [
     {
       path: '/',
